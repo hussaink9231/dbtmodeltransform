7d953708e9319da60cd78b2f70e1ce71d7beff93
Kaja Hussain <hussain.k2394@gmail.com>
2023-10-28 01:43:01 +0530

    add models

--- models/customer.sql
+++ models/customer.sql
@@ -1,15 +1,20 @@
 {{ config(schema='demo') }}
 SELECT cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CUSTOMER_SK') as Numeric(38,0))
 as C_CUSTOMER_SK,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_CDEMO_SK') as Numeric(38,0) )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_CDEMO_SK')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_CDEMO_SK') as Numeric(38,0)) end
 as C_CURRENT_CDEMO_SK,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_HDEMO_SK') as Numeric(38,0) )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_HDEMO_SK')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_HDEMO_SK') as Numeric(38,0)) end
 as C_CURRENT_HDEMO_SK,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_ADDR_SK') as Numeric(38,0) )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_ADDR_SK')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_CURRENT_ADDR_SK') as Numeric(38,0)) end
 as C_CURRENT_ADDR_SK,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_FIRST_SHIPTO_DATE_SK') as Numeric(38,0) )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_FIRST_SHIPTO_DATE_SK')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_FIRST_SHIPTO_DATE_SK') as Numeric(38,0)) end
 as C_FIRST_SHIPTO_DATE_SK,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_FIRST_SALES_DATE_SK') as Numeric(38,0) )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_FIRST_SALES_DATE_SK')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_FIRST_SALES_DATE_SK') as Numeric(38,0)) end
 as C_FIRST_SALES_DATE_SK,
 cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_SALUTATION') as VARCHAR(10) )
 as C_SALUTATION,
@@ -19,11 +24,14 @@ cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_LAST_NAME') as VARC
 as C_LAST_NAME,
 cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_PREFERRED_CUST_FLAG') as VARCHAR(1) )
 as C_PREFERRED_CUST_FLAG,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_DAY') as Numeric(38,0) )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_DAY')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_DAY') as Numeric(38,0)) end
 as C_BIRTH_DAY,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_MONTH') as  Numeric(38,0)  )
+case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_MONTH')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_MONTH') as Numeric(38,0)) end
 as C_BIRTH_MONTH,
-cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_YEAR') as Numeric(38,0)  )
+ case when (JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_YEAR')) = '' then null
+else  cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_YEAR') as Numeric(38,0)) end
 as C_BIRTH_YEAR,
 cast(JSON_EXTRACT_PATH_TEXT(JSON_SERIALIZE(_airbyte_data),'C_BIRTH_COUNTRY') as VARCHAR(20) )
 as C_BIRTH_COUNTRY,
